a3da0efd92dd145139a8bfff862fe74483fa775a
Jonathan Chosson <catstrike@gmail.com>
2021-02-05 10:53:14 +0100

    [Build Style JS] app Hobbies :dizzy: Mise en place stylisage et JS de Hobbies

--- js/index.js
+++ js/index.js
@@ -61,3 +61,52 @@ btnHardSkills.addEventListener('click', function(){
 retourHardSkills.addEventListener('click', function(){
     masquageApp(hardSkills);
 })
+
+//affiche et masque #hobbies
+let btnHobbies = document.getElementById('btnHobbies');
+let retourHobbies = document.getElementById('retourHobbies');
+
+btnHobbies.addEventListener('click', function(){
+    affichageApp(hobbies);
+})
+
+retourHobbies.addEventListener('click', function(){
+    masquageApp(hobbies);
+})
+
+// Caroussel Hobbies //
+
+const images = document.querySelectorAll('.caroussel');
+let imgActive = 0;
+
+//rend toute les images invisibles sauf la première
+for( let i = 1; i < images.length; i++) {
+    images[i].classList.add('invisible');
+};
+
+// Ecoute sur clic btn suivant
+document.getElementById('suivant').addEventListener('click', function(){
+    images[imgActive].classList.add('invisible');
+    images[imgActive].classList.remove('slide-in-left');
+    images[imgActive].classList.remove('slide-in-right');
+    imgActive ++;
+    if(imgActive >= images.length){
+        imgActive = 0;
+    }
+    images[imgActive].classList.remove('invisible');
+    images[imgActive].classList.add('slide-in-right');
+});
+
+// Ecoute sur bouton precedant
+document.getElementById('precedant').addEventListener('click', function(){
+    images[imgActive].classList.add('invisible');
+    images[imgActive].classList.remove('slide-in-left');
+    images[imgActive].classList.remove('slide-in-right');
+    imgActive += -1;
+    if(imgActive < 0){
+        imgActive = images.length;
+        imgActive += -1;
+    }
+    images[imgActive].classList.remove('invisible');
+    images[imgActive].classList.add('slide-in-left');
+})
